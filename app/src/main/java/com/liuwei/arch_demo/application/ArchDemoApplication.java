0dcc7f2ef739bed31f5e67204f6f2f502c5fe449
wei.liu <wei.liu@thundersoft.com>
2022-08-30 19:38:58 +0800

    第六次提交
    增加Application生命周期 介绍

--- app/src/main/java/com/liuwei/arch_demo/application/ArchDemoApplication.java
+++ app/src/main/java/com/liuwei/arch_demo/application/ArchDemoApplication.java
@@ -1,6 +1,7 @@
 package com.liuwei.arch_demo.application;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.liuwei.base.preference.PreferencesUtil;
 import com.liuwei.network.base.NetworkApi;
@@ -8,10 +9,36 @@ import com.liuwei.network.base.NetworkApi;
 
 public class ArchDemoApplication extends Application {
 
+    private static String TAG = "LifeCycleApplication";
+
     @Override
     public void onCreate() {
+        // 程序创建的时候执行
+        Log.d(TAG, "onCreate");
         super.onCreate();
         NetworkApi.init(new NetworkRequestInfo(this));
         PreferencesUtil.init(this);
+    }
+
+    @Override
+    public void onTerminate() {
+        // 程序终止的时候执行
+        Log.d(TAG, "onTerminate");
+        super.onTerminate();
+    }
+
+    @Override
+    public void onLowMemory() {
+        // 低内存的时候执行
+        Log.d(TAG, "onLowMemory");
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        // 程序在内存清理的时候执行（回收内存）
+        // HOME键退出应用程序、长按MENU键，打开Recent TASK都会执行
+        Log.d(TAG, "onTrimMemory");
+        super.onTrimMemory(level);
     }
 }
